8712d6a6ffb70758a2e2a3ecd06f4b265188c789
Jan Rehorik <jrehorik@serviceobjects.com>
2025-09-26 16:05:54 -0700

    typo

--- lead-validation-international-dot-net-examples/Main.cs
+++ lead-validation-international-dot-net-examples/Main.cs
@@ -3,7 +3,7 @@
 //Your license key from Service Objects.
 //Trial license keys will only work on the
 //trail environments and production license
-//keys will only owork on production environments.
+//keys will only work on production environments.
 string LicenseKey = "LICENSE KEY";
 
 bool IsProductionKey = false;
